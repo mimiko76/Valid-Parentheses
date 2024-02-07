552729a2800d1dece9996e14f2fdc799101e57f1
mimiko76 <tema.levin2016@gmail.com>
2024-02-07 18:31:26 +0300

    Добавьте файлы проекта.

---
+++ Practics/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practics
+{
+    internal class Program
+    {
+
+        public static bool IsValid(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char c in s)
+            {
+                if (c == '(' || c == '{' || c == '[')
+                {
+                    stack.Push(c);
+                }
+                else if (c == ')' && (stack.Count == 0 || stack.Pop() != '('))
+                {
+                    return false;
+                }
+                else if (c == '}' && (stack.Count == 0 || stack.Pop() != '{'))
+                {
+                    return false;
+                }
+                else if (c == ']' && (stack.Count == 0 || stack.Pop() != '['))
+                {
+                    return false;
+                }
+            }
+
+            return stack.Count == 0; 
+        }
+
+        public static void Main()
+        {
+            string input = "()";
+            bool isValid = IsValid(input); // Проверяем допустимость введенной строки
+
+            if (isValid)
+            {
+                Console.WriteLine("Введенная строка допустима.");
+            }
+            else
+            {
+                Console.WriteLine("Введенная строка не допустима.");
+            }
+        }
+    }
+}
---
+++ Practics/cart.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practics
+{
+    internal class cart
+    {
+        public static Random rnd = new Random();
+        int[] coloda = {6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,11,11,11,11,5,5,5,5,4,4,4,4,3,3,3,3};
+        int getCart(int[] coloda)
+        {
+            string nameCart = string.Empty;
+            int result = rnd.Next(3, 12);
+            if (result == 3) nameCart = "Валет";
+            if (result == 4) nameCart = "Дама";
+            if (result == 5) nameCart = "Король";
+            if (result == 11) nameCart = "Туз";
+            return result;
+        }
+    }
+}
